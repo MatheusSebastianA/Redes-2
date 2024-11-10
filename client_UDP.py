ee2508e0b9be00deb3050874e30333040af10423
Matheus <matheussebastian@ufpr.br>
2024-11-10 10:22:13 -0500

    Monitoramento do tempo em conexao UDP basica entre VM e maquina local

--- client_UDP.py
+++ client_UDP.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 def cliente_udp(ip_servidor, porta):
     # Define o endereço do servidor
@@ -6,12 +7,18 @@ def cliente_udp(ip_servidor, porta):
 
     # Cria o socket UDP
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as cliente_socket:
-        dados = b"Exemplo de dados para envio via UDP"
-        
-        # Envia dados 1000 vezes para o servidor
-        for _ in range(1000):
+        dados = b"Oi\n"
+        start_time = time.time()
+
+        # Envia dados 100 vezes para o servidor
+        for _ in range(100):
             cliente_socket.sendto(dados, endereco_servidor)
+
         print("Dados enviados.")
+    
+        end_time = time.time()
+        tempo_total = end_time - start_time
+        print(f"Tempo total: {tempo_total} segundos.")
 
 # Executa o cliente
 cliente_udp("192.168.5.137", 5001)
